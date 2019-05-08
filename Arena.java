5415e2d7f12df67b16f4ca7699da2c76f6aae229
nvco10 <48620419+nvco10@users.noreply.github.com>
2019-05-08 18:16:26 +0200

    Add files via upload
    Ball Korb Aktion im Moment

--- Arena.java
+++ Arena.java
@@ -16,22 +16,29 @@ public class Arena extends World
     public Arena()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 450, 1); 
+
         
-        Ball = new Ball();
-        addObject(Ball, 100, 150);
+        Korb k1 = new Korb();
+        addObject(k1, 550, 100);
+                
+      
         
-        Korb = new Korb();
-        addObject(Korb, 600, 300);
+        Spieler s = new Spieler();
+        addObject(s, 150, 320);
         
-        Spieler = new Spieler();
-        addObject(Spieler, 100, 100);
+              
+        Kraftanzeige kr = new Kraftanzeige();
+        addObject(kr, 550, 350);
         
-        Richtungsanzeige = new Richtungsanzeige();
-        addObject(Richtungsanzeige, 400, 25);
+        Richtungsanzeige r = new Richtungsanzeige();
+        addObject(r, 150, 300);
         
-        Kraftanzeige = new Kraftanzeige();
-        addObject(Kraftanzeige, 550, 25);
+        Ball b = new Ball(k1);
+        addObject(b, 155, 300);
+     
+
+
     }
     public void loescheObjekte()
     {
@@ -39,4 +46,5 @@ public class Arena extends World
         removeObjects(objekte);
         setBackground("Bild");
     }
+
 }
--- Ball.java
+++ Ball.java
@@ -12,28 +12,43 @@ public class Ball extends Actor
      * Act - do whatever the Ball wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    int i = 0;
+   
     double t =0;
+    Korb k;
+    int i = 1;
+    
     public void act() 
     {
         // Add your action code here.
-       bewegen();
+     
+       
+        bewegen();
+        
+   
     
     }  
  
-    public Ball()
+    public Ball(Korb k1)
     {
-    
+        k = k1;
     }
     
     public void bewegen()
     {
-       double x, y;
-    int v0=50;     //Anfangsgeschwindigkeit
+    double x, y;
+    int v0=100;     //Anfangsgeschwindigkeit
 
-    double winkel = 70 * (6.283185307/360);
+    double winkel = 40 * (6.283185307/360);
     
     int g=10;
+    
+   
+    if( berührt() == true)
+    {i++;}
+    
+    if( i== 1)
+    {
+
 
     t=t+0.1;
     
@@ -43,5 +58,25 @@ public class Ball extends Actor
     int x0 = (int) Math.round(x);
     int y0 = (int) Math.round(y);
     setLocation(x0,y0);
+     
+}
+if(i == 2)
+{
+    t=t-0.1;
+    
+    x= 150 + v0*Math.cos(winkel)*t;
+    y= 300 - v0*Math.sin(winkel)*t+(g/2)*(t*t);
+    
+    int x0 = (int) Math.round(x);
+    int y0 = (int) Math.round(y);
+    setLocation(x0,y0);
+
+}
+    }
+    public boolean berührt()
+    {
+       
+        boolean c = intersects(k);
+        return c;
     }
 }
